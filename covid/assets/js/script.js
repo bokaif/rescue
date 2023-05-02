971880c6670b71d9c30a6c3bba72093ba71e24cf
bokaif <chiki.monk3y@gmail.com>
2023-05-03 02:26:58 +0600

    Added language selector & Fixed the result part

--- covid/assets/js/script.js
+++ covid/assets/js/script.js
@@ -1,14 +1,13 @@
 $(".btn-long").hide();
 $(".cimg").hide();
-var check=0
+var check = 0
 
-$(".yes").click(function(){
-    check=check+1;
+$(".yes").click(function () {
+    check = check + 1;
 });
-$(".no").click(function(){
-    check=check-1;
+$(".no").click(function () {
+    check = check - 1;
 });
-
 
 var questions_bn = [
     "আপনার কাশি আছে?",
@@ -23,7 +22,7 @@ var questions_bn = [
     "আপনি কি গত ১৪ দিনের মধ্যে সম্প্রতি ভ্রমণ করেছেন?",
     "আপনি কি কোভিড-১৯ সংক্রমিত কোনো এলাকায় গিয়েছেন?",
     "আপনার সাথে কি কোনো কোভিড-১৯ রোগীর সরাসরি যোগাযোগ রয়েছে বা যত্ন নিয়েছেন?",
-    ];
+];
 
 var questions_en = [
     "Do you have a cough?",
@@ -38,7 +37,7 @@ var questions_en = [
     "Have you traveled recently in the last 14 days?",
     "Have you been to an area infected with Covid-19?",
     "Do you have direct contact with any Covid-19 patient or did you take care of them?",
-    ];
+];
 
 var images = [
     "cough",
@@ -53,33 +52,69 @@ var images = [
     "travel",
     "covid_area",
     "covid_patient",
-    ];
+];
 
-
+var urlParams = new URLSearchParams(window.location.search);
+var lang = urlParams.get('lang');
+if (lang == "bn") {
+    $(".language .en").removeClass("active")
+    $(".language .bn").addClass("active")
+    $(".form").attr("lang", "bn");
+    var questions = questions_bn;
+    $(".ques").html("কোভিড-১৯ স্ব-মূল্যায়ন পরীক্ষা শুরু করতে নিচের বাটনটি প্রেস করুন।");
+    $(".start").html("শুরু করুন");
+    $(".yes").html("হ্যাঁ");
+    $(".no").html("না");
+} else {
+    $(".form").attr("lang", "en");
+    var questions = questions_en;
+}
 
 var question_count = -1;
 
-$(".start").click(function() {
+const riskThresholds = {
+    "mild": 1,
+    "moderate": 5,
+    "severe": 10,
+};
+
+$(".start").click(function () {
     $('.topimg').css("background-image", "url('assets/svg/logo.svg')");
     $('.topimg').css("height", "70px");
     $(".start").hide();
     $(".cimg").css("margin-top", "25px");
     $(".cimg").show();
     $(".btn-long").show();
-    $(".btn-long").click(function() {
+    $(".btn-long").click(function () {
         console.log(check);
     });
 });
 
-$(".btn").click(function() {
+$(".btn").click(function () {
     question_count++;
-    $(".cimg").css("background-image", "url('assets/svg/"+images[question_count]+".svg')");
-    $(".ques").html(questions_bn[question_count]);
+    $(".cimg").css("background-image", "url('assets/svg/" + images[question_count] + ".svg')");
+    $(".ques").html(questions[question_count]);
 
-    if (question_count >= 12){
-        $(".ques").hide();
-        $(".cimg").hide();
+    if (question_count >= questions.length) {
+        let riskLevel, qClass;
+        if (check < riskThresholds.mild) {
+            riskLevel = lang === "bn" ? "সামান্য" : "Mild";
+            qClass = "mild";
+        } else if (check < riskThresholds.moderate) {
+            riskLevel = lang === "bn" ? "মাঝারি" : "Moderate";
+            qClass = "moderate";
+        } else {
+            riskLevel = lang === "bn" ? "গুরুতর" : "Severe";
+            qClass = "severe";
+        }
+
+        // display the result to the user
+        $(".ques").html(lang === "bn" ? `আপনার ঝুঁকির মাত্রা: <span class=${qClass}>${riskLevel}</span>` : `Your Risk Level: <span class=${qClass}>${riskLevel}</span>`);
+        $(".btn").show();
         $(".btn-long").hide();
+        $(".btn").html(lang === "bn" ? "রিসেট <i class='fa-light fa-arrow-rotate-left'></i>" : "Reset <i class='fa-light fa-arrow-rotate-left'></i>");
+        $(".btn").click(function () {
+            location.reload();
+        });
     }
 });
-
